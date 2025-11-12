18a87a7ea19070480db838abaf5885fc7c0f56c1
yara <54339485+yarstn@users.noreply.github.com>
2025-11-13 02:03:50 +0300

    Delete Ecommerce directory

--- Ecommerce/Sources/Ecommerce/Auth/AdminGuard.swift
+++
@@ -1,9 +0,0 @@
-import Vapor
-
-struct AdminGuard: AsyncMiddleware {
-    func respond(to req: Request, chainingTo next: AsyncResponder) async throws -> Response {
-        let user = try req.auth.require(User.self)
-        guard user.isAdmin == true else { throw Abort(.forbidden, reason: "Admins only") }
-        return try await next.respond(to: req)
-    }
-}
