d7df7e93c5d9fb81a18846bc07cdcd36d4f2d7b5
yara <54339485+yarstn@users.noreply.github.com>
2025-11-13 17:19:04 +0300

    Update AdminGuard.swift

--- Sources/Ecommerce/Auth/AdminGuard.swift
+++ Sources/Ecommerce/Auth/AdminGuard.swift
@@ -1,9 +1,14 @@
 import Vapor
 
 struct AdminGuard: AsyncMiddleware {
-func respond(to req: Request, chainingTo next: any AsyncResponder) async throws -> Response
+    func respond(
+        to req: Request,
+        chainingTo next: any AsyncResponder
+    ) async throws -> Response {
         let user = try req.auth.require(User.self)
-        guard user.isAdmin == true else { throw Abort(.forbidden, reason: "Admins only") }
+        guard user.isAdmin == true else {
+            throw Abort(.forbidden, reason: "Admins only")
+        }
         return try await next.respond(to: req)
     }
 }
